b7de4ccac599686f50882d28448798e59fbf7a87
Eric Rovell <ericrovell@gmail.com>
2019-02-05 13:54:31 +0300

    del junk

--- _temp/__.js
+++
@@ -1,5 +0,0 @@
-function isPrime(number) {
-  return !('1'.repeat(number).match(/^1?$|^(11+?)\1+$/))
-}
-
-console.log(isPrime(239))
--- _temp/long_division.py
+++
@@ -1,6 +0,0 @@
-def decimals(numerator, denominator):    
-  while denominator:      
-    yield denominator // numerator
-    denominator = denominator % numerator * 10
-
-print(list(decimals(3, 2)))
