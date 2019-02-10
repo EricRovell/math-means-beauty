0f24a9976a071fa6082b475fa626d434eaf5c9f8
Eric Rovell <ericrovell@gmail.com>
2019-02-10 12:27:39 +0300

    sieve of Atkin, python

---
+++ python/functions/asieve.py
@@ -0,0 +1,41 @@
+# sieve of Atkin
+def asieve(limit):
+  assert limit > 3
+  sieve = [False] * (limit + 1)
+  sieve[2:4] = (True, True)
+
+  # preliminary work
+  x = 1
+  while x**2 < limit:
+    y = 1
+    while y**2 < limit:
+
+      n = 4 * x**2 + y**2
+      if n <= limit and n % 12 in (1, 5):
+        sieve[n] = not sieve[n]
+
+      n = 3 * x**2 + y**2
+      if n <= limit and n % 12 == 7:
+        sieve[n] = not sieve[n]
+
+      if x > y:
+        n = 3 * x**2 - y**2
+        if n <= limit and n % 12 == 11:
+          sieve[n] = not sieve[n]
+
+      y += 1
+    x += 1
+    
+  # remove the squares of primes (and their multiples)
+  r = 5
+  while r**2 < limit:
+    if sieve[r]:
+      for n in range(r**2, len(sieve), r**2):
+        sieve[n] = False
+    r += 1
+    
+  # append everything into a list
+  return [value for value, isprime in enumerate(sieve) if isprime]
+
+# tests
+print(len(asieve(10000)))
